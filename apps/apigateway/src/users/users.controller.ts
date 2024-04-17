a957729da76d45c1ca0deb9311768394a4eb47ec
solomon akinbiyi <akinbiyisolo22@gmail.com>
2024-04-17 20:40:25 +0100

    done with user auth

--- apps/apigateway/src/users/users.controller.ts
+++ apps/apigateway/src/users/users.controller.ts
@@ -1,7 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from '@app/common';
+import { UpdateUserDto } from '@app/common';
 
 @Controller('users')
 export class UsersController {
@@ -19,16 +27,16 @@ export class UsersController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+    return this.usersService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+    return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+    return this.usersService.remove(id);
   }
 }
---
+++ apps/apigateway/src/users/users.service.ts
@@ -0,0 +1,41 @@
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import {
+  CreateUserDto,
+  UpdateUserDto,
+  USER_SERVICE_NAME,
+  UserServiceClient,
+} from '@app/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { AUTH_SERVICE } from './constants';
+
+@Injectable()
+export class UsersService implements OnModuleInit {
+  private usersService: UserServiceClient;
+
+  constructor(@Inject(AUTH_SERVICE) private client: ClientGrpc) {}
+
+  onModuleInit() {
+    this.usersService =
+      this.client.getService<UserServiceClient>(USER_SERVICE_NAME);
+  }
+
+  create(createUserDto: CreateUserDto) {
+    return this.usersService.createUser(createUserDto);
+  }
+
+  findAll() {
+    return this.usersService.findAllUsers({});
+  }
+
+  findOne(id: string) {
+    return this.usersService.findOneUser({ id });
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.usersService.updateUser({ id, ...updateUserDto });
+  }
+
+  remove(id: string) {
+    return this.usersService.removeUser({ id });
+  }
+}
